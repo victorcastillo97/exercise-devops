7d06252a4f0bc1bc2395f0877a1dfd2be9150424
victor <vprotecso@pop-os.localdomain>
2022-08-27 17:20:13 -0500

    complete.test.server

--- app/tests/server.test.js
+++ app/tests/server.test.js
@@ -1,14 +1,25 @@
 import request from 'supertest';
 import app from '../server.js';
 
-
-
 it('Respond with json', done => {
     request(app)
         .get('/retoibm/sumar/0/0')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200,done);
+});
+
+it('Is the server healthy?', done => {
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect('{"status":{"healthy":true}}')
+        .end((err) => {
+            if (err) return done(err);
+            done();
+        })
 });
 
 it('Respond correct', done => {
