ad8d656c278909f1b5b3b1a21210a531c5542b2b
victor <vprotecso@pop-os.localdomain>
2022-08-27 16:04:45 -0500

    build.test

--- app/src/app.js
+++ app/src/app.js
@@ -3,17 +3,22 @@ import _express  from 'express';
 
 const _server = _express();
 
+
+_server.get('/health', function(request, response) {
+    response.status(200).json({resultado : "The server is healthy"});
+});
+
 _server.get('/retoibm/sumar/:sumando01/:sumando02', function(request, response) {
   try{
-    var _sumando01 = new Number(request.params.sumando01);
-    var _sumando02 = new Number(request.params.sumando02);
-    var _resultado = _sumando01 + _sumando02;
+        var _sumando01 = new Number(request.params.sumando01);
+        var _sumando02 = new Number(request.params.sumando02);
+        var _resultado = _sumando01 + _sumando02;
 
     if (typeof _resultado !== "undefined" && _resultado!==null && !isNaN(_resultado)){
         record_data(_sumando01,_sumando02,_resultado)
-      return response.status(200).json({resultado : _resultado});
+        return response.status(200).json({resultado : _resultado});
     }else{
-      return response.status(400).json({resultado : "Bad Request"});
+        return response.status(400).json({resultado : "Bad Request"});
     }
   }
   catch(e){
