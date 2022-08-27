6ad5618ec217f542135538ac2f9667e87b487213
victor <vprotecso@pop-os.localdomain>
2022-08-27 13:03:40 -0500

    ecr.config.containers

--- Makefile
+++ Makefile
@@ -1,34 +1,12 @@
 include makefiles/ssh.mk
+include makefiles/ecr.mk
 
-NAME_APP = exercise
-APP_DIR = app
-ENV = dev
+TYPE_APP 		= exercise
+SERVICE_NAME 	= devops
+ENV 			= dev
+PROJECT_NAME 	= ${TYPE_APP}-${SERVICE_NAME}-${ENV}
+SERVER_NUMBER_APP = 1
 
-
-PROJECT_NAME = ${NAME_APP}-${ENV}
-IMAGE_NAME_APP = ${NAME_APP}:latest
-IMAGE_NAME_BD = app.bd:latest
-IMAGE_NAME_PROXY = proxy.bd:latest
-
-build.image.app:
-	@ docker build  \
-		-f docker/app/Dockerfile \
-		-t ${IMAGE_NAME_APP} \
-		. \
-
-build.image.bd:
-	@ docker build  \
-		-f docker/bd/Dockerfile \
-		-t ${IMAGE_NAME_BD} \
-		. \
-		--no-cache
-
-build.image.proxy:
-	@ docker build  \
-		-f docker/proxy/Dockerfile \
-		-t ${IMAGE_NAME_PROXY} \
-		. \
-		--no-cache
 
 run.app.local:
 	@ docker run -d \
@@ -37,11 +15,15 @@ run.app.local:
 
 run.nginx.local:
 	@ docker run -d \
-	-p 9090:80 \
+	-p 9090:4000 \
 	${IMAGE_NAME_PROXY} \
-
 
 run:
 	@ docker run --rm -v app/conf/proxy.conf:/etc/nginx/conf.d/default.conf ngnix
 
+up:
+	@cd docker && docker compose up --scale api=${SERVER_NUMBER_APP}
+
+down:
+	@cd docker && docker compose down
 
--- makefiles/ecr.mk
+++ makefiles/ecr.mk
@@ -3,9 +3,16 @@
 		ecr.login \
 		ecr.push \
 
-REPOSITORY = app
+
+NAME_APP = app-${PROJECT_NAME}
+NAME_BD = bd-${PROJECT_NAME}
+NAME_PROXY = proxy-${PROJECT_NAME}
+
+IMAGE_APP = ${NAME_APP}:latest
+IMAGE_BD = ${NAME_BD}:latest
+IMAGE_PROXY = ${NAME_PROXY}:latest
+
 ECR_REGION = us-west-2
-IMAGE = exercise
 
 envs:
 	$(eval AWS_ACCOUNT_ID = $(shell aws sts get-caller-identity --query "Account" --output text))
@@ -30,14 +37,55 @@ ecr.create: COMMAND=$(shell output=$$(make ecr.consult 2>&1); \
 ecr.create:
 	@echo $(COMMAND)
 
-
 ecr.login: envs
 	@aws ecr get-login-password --region ${ECR_REGION} --profile default | \
 	docker login --username AWS --password-stdin \
 	${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com
 
 
+build.image.app:
+	@ docker build  \
+		-f docker/app/Dockerfile \
+		-t ${IMAGE_APP} \
+		. \
+		--no-cache
 
+build.image.bd:
+	@ docker build  \
+		-f docker/bd/Dockerfile \
+		-t ${IMAGE_BD} \
+		. \
+		--no-cache
+
+build.image.proxy:
+	@ docker build  \
+		-f docker/proxy/Dockerfile \
+		-t ${IMAGE_PROXY} \
+		docker/proxy/ \
+		--no-cache
+
+ecr.tag.image: envs
+	@docker tag ${IMAGE} ${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${IMAGE}
 
 ecr.push: envs
-	@docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${IMAGE}:latest
+	@docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${IMAGE}
+
+ecr.push.app:
+	@ export REPOSITORY=${NAME_APP} && make ecr.create && \
+		make ecr.tag.image IMAGE=${IMAGE_APP} && \
+		make ecr.push IMAGE=${IMAGE_APP}
+
+ecr.push.bd:
+	@ export REPOSITORY=${NAME_BD} && make ecr.create && \
+		make ecr.tag.image IMAGE=${IMAGE_BD} && \
+		make ecr.push IMAGE=${IMAGE_BD}
+
+ecr.push.proxy:
+	@ export REPOSITORY=${NAME_PROXY} && make ecr.create && \
+		make ecr.tag.image IMAGE=${IMAGE_PROXY} && \
+		make ecr.push IMAGE=${IMAGE_PROXY}
+
+ecr.destroy.images:
+	@for number in ${NAME_APP} ${NAME_BD} ${NAME_PROXY} ; do \
+        export REPOSITORY=$$number && make ecr.destroy ; \
+    done
