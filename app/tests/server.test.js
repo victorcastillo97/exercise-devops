5952d30f2d0460b7afd68f237c59ab2863956f43
victor <vprotecso@pop-os.localdomain>
2022-08-27 16:04:45 -0500

    build.test

--- app/tests/server.test.js
+++ app/tests/server.test.js
@@ -1,10 +1,50 @@
-const request = require('supertest')
-const app = require('../server.js')
+import request from 'supertest';
+import app from '../server.js';
 
-it('respond with json', done => {
+it('Is the server healthy?', done => {
     request(app)
-        .get('/retoibm/sumar/4/50')
+        .get('/health')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect('{"resultado":"The server is healthy"}')
+        .end((err) => {
+            if (err) return done(err);
+            done();
+        })
+});
+
+it('Respond with json', done => {
+    request(app)
+        .get('/retoibm/sumar/0/0')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200,done);
-})
+});
+
+it('Respond correct', done => {
+    request(app)
+        .get('/retoibm/sumar/999999999999999/999999999999999')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect('{"resultado":1999999999999998}')
+        .end((err) => {
+            if (err) return done(err);
+            done();
+        })
+});
+
+
+it('Correctly answering the incorrect', done => {
+    request(app)
+        .get('/retoibm/sumar/comida/basura')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .expect('{"resultado":"Bad Request"}')
+        .end((err) => {
+            if (err) return done(err);
+            done();
+        })
+});
